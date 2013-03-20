eb03bf90aab66a7e2923f3ff6e6b0b0edc72bee3
bootstraponline <code@bootstraponline.com>
2013-03-20 12:41:59 -0300

    Fix add_to_path

--- lib/app_lib.rb
+++ lib/app_lib.rb
@@ -1,9 +1,10 @@
-def self.add_to_path path
- path = File.expand_path "../#{path}/", __FILE__
+def self.add_to_path file, path=false
+ path = path ? "../#{path}/" : '..' 
+ path = File.expand_path path, file
 
  $:.unshift path unless $:.include? path
 end
 
-add_to_path 'app_lib'
+add_to_path __FILE__, 'app_lib'
 
 require 'console'
