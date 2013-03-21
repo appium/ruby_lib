48125a62723ff1206fb753f46194c33f5afd68a8
bootstraponline <code@bootstraponline.com>
2013-03-21 16:49:32 -0400

    Release 0.0.17

--- lib/app_lib/version.rb
+++ lib/app_lib/version.rb
@@ -1,4 +1,4 @@
 module AppLib
-  VERSION = '0.0.16' unless defined? ::AppLib::VERSION
-  DATE = '2013-03-20' unless defined? ::AppLib::DATE
+  VERSION = '0.0.17' unless defined? ::AppLib::VERSION
+  DATE = '2013-03-21' unless defined? ::AppLib::DATE
 end
