73b757cd28c5bc85b98d6e747f79f7ee33af5022
bootstraponline <code@bootstraponline.com>
2013-04-05 13:47:30 -0400

    Define find and finds to search everything

--- lib/appium_lib/element/ios/generic.rb
+++ lib/appium_lib/element/ios/generic.rb
@@ -9,26 +9,26 @@ This differs from Android where name (the content description) is empty when not
 name defaults to label when undefined. value is never a default so that must be
 included in a new search.
 
+Find - search everything.
+
 The search order is:
 1. name
 2. label (implied by name)
 3. value
+
 =end
 
 # Return the first element matching text.
 # @param text [String] the text to search for
 # @return [Element] the first matching element
-def text text
+def find text
   # returnElems requires a wrapped $(element).
   # set to empty array when length is zero to prevent hang.
   js = %Q(
     var eles = au.mainWindow.elements();
-    var a = eles.firstWithPredicate("name contains[c] '#{text}'");
+    var a = eles.firstWithPredicate("name contains[c] '#{text}' || label contains[c] '#{text}' || value contains[c] '#{text}'");
     if ( a.length === 0 ) {
-      a = eles.firstWithPredicate("value contains[c] '#{text}'");
-      if ( a.length === 0 ) {
-        a = [];
-      }
+      a = [];
     }
     au._returnElems($(a));
   )
@@ -39,13 +39,12 @@ end
 # Return all elements matching text.
 # @param text [String] the text to search for
 # @return [Array<Element>] all matching elements
-def texts text
+def finds text
   # returnElems requires a wrapped $(element).
   # must call toArray when using withPredicate instead of firstWithPredicate.
   js = %Q(
     var eles = au.mainWindow.elements();
-    var a = eles.withPredicate("name contains[c] '#{text}'").toArray();
-    a = a.concat( eles.withPredicate("value contains[c] '#{text}'").toArray() );
+    var a = eles.withPredicate("name contains[c] '#{text}' || label contains[c] '#{text}' || value contains[c] '#{text}'").toArray();
     if ( a.length === 0 ) {
       a = [];
     }
@@ -53,24 +52,6 @@ def texts text
   )
 
   execute_script js
-end
-
-# Return the first element matching name.
-# on Android name is content description
-# on iOS name is the accessibility label or the text.
-# @param name [String] the name to search for
-# @return [Element] the first matching element
-def name name
-  text name
-end
-
-# Return all elements matching name.
-# on Android name is content description
-# on iOS name is the accessibility label or the text.
-# @param name [String] the name to search for
-# @return [Array<Element>] all matching elements
-def names name
-  texts name
 end
 
 end # if ios
