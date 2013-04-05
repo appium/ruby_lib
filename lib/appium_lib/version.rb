fb4af206c114cf8f75fcb41cdbbea0ba728bf7e6
bootstraponline <code@bootstraponline.com>
2013-04-05 13:00:48 -0400

    Release 0.0.27

--- lib/appium_lib/version.rb
+++ lib/appium_lib/version.rb
@@ -1,4 +1,4 @@
 module AppiumLib
-  VERSION = '0.0.26' unless defined? ::AppiumLib::VERSION
-  DATE = '2013-04-04' unless defined? ::AppiumLib::DATE
+  VERSION = '0.0.27' unless defined? ::AppiumLib::VERSION
+  DATE = '2013-04-05' unless defined? ::AppiumLib::DATE
 end
