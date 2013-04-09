e1f470e0a2b9a6cf94ffa20edbc244e31c0375e8
bootstraponline <code@bootstraponline.com>
2013-04-09 14:09:42 -0300

    Add find for Android

--- lib/appium_lib/element/android/generic.rb
+++ lib/appium_lib/element/android/generic.rb
@@ -10,6 +10,47 @@ find_element :name by default uses a partial case insensitive match.
 On iOS the default is an exact name match.
 =end
 
+=begin
+    // iOS version
+    // https://github.com/appium/ruby_lib/blob/37bb4e90b29e5adb4438b287b6387a504c94b5c4/lib/appium_lib/element/ios/generic.rb#L23
+    var search = "name contains[c] '#{text}' || label contains[c] '#{text}' || value contains[c] '#{text}'";
+    var a = w.secureTextFields().firstWithPredicate(search);
+    if ( isNil(a) ) {
+      a = w.textFields().firstWithPredicate(search);
+      if ( isNil(a) ) {
+        a = w.buttons().firstWithPredicate(search);
+        if ( isNil(a) ) {
+          a = w.elements().firstWithPredicate(search);
+        }
+      }
+    }
+
+Android considers both a textfield and a secure textfield to be "EditText".
+Name (the content desc) is searched first and then we search for value (text).
+There's no label in Android.
+
+Android buttons have different class names (android.widget.Button, android.widget.ImageButton)
+so we consider the element a button if the class name contains the word button.
+
+After looking for textfields and buttons, then we search all elements. Find will return
+the first element that matches.
+=end
+def find val
+  # s.className('android.widget.EditText').descriptionContains(value);
+  args = [ [4, 'android.widget.EditText'], [7, val] ],
+  # s.className('android.widget.EditText').textContains(value);
+         [ [4, 'android.widget.EditText'], [3, val] ],
+  # s.classNameMatches('(?i).*button.*').descriptionContains(value);
+         [ [26, '(?i).*button.*'], [7, val] ],
+  # s.classNameMatches('(?i).*button.*').textContains(value);
+         [ [26, '(?i).*button.*'], [3, val] ],
+  # s.descriptionContains(value);
+         [ [7, val] ],
+  # s.textContains(value);
+         [ [3, val] ]
+  mobile :find, args
+end
+
 # Return the first element matching text.
 # @param text [String] the text to search for
 # @return [Element] the first matching element
