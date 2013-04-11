7676a90af8a1951817fcc8c49ddef46577cf5726
bootstraponline <code@bootstraponline.com>
2013-04-11 10:21:55 -0400

    Add iOS name, names, text, texts
    Fix #13

--- lib/appium_lib/element/ios/generic.rb
+++ lib/appium_lib/element/ios/generic.rb
@@ -15,12 +15,12 @@ The search order is:
 1. name
 2. label (implied by name)
 3. value
+
+Android name = iOS name & label
+Android text = iOS value
 =end
 
-# Return the first element matching text.
-# @param text [String] the text to search for
-# @return [Element] the first matching element
-def find text
+def first_ele_js predicate
   # returnElems requires a wrapped $(element).
   # set to empty array when length is zero to prevent hang.
   #
@@ -30,13 +30,13 @@ def find text
   # 2. textFields
   # 3. buttons
   # 4. elements
-  js = %Q(
+  %Q(
     function isNil( a ) {
       return a.type() === 'UIAElementNil';
     }
 
     var w = au.mainWindow;
-    var search = "name contains[c] '#{text}' || label contains[c] '#{text}' || value contains[c] '#{text}'";
+    var search = #{predicate};
     var a = w.secureTextFields().firstWithPredicate(search);
     if ( isNil(a) ) {
       a = w.textFields().firstWithPredicate(search);
@@ -54,6 +54,27 @@ def find text
 
     au._returnElems($(a));
   )
+end
+
+def all_ele_js predicate
+  %Q(
+    var w = au.mainWindow;
+    var search = #{predicate};
+    var a = w.elements().withPredicate(search).toArray();
+
+    if ( a.length === 0 ) {
+      a = [];
+    }
+
+    au._returnElems($(a));
+  )
+end
+
+# Return the first element matching text.
+# @param text [String] the text to search for
+# @return [Element] the first matching element
+def find text
+  js = first_ele_js "name contains[c] '#{text}' || label contains[c] '#{text}' || value contains[c] '#{text}'"
 
   execute_script(js).first
 end
@@ -64,17 +85,54 @@ end
 def finds text
   # returnElems requires a wrapped $(element).
   # must call toArray when using withPredicate instead of firstWithPredicate.
-  js = %Q(
-    var w = au.mainWindow;
-    var search = "name contains[c] '#{text}' || label contains[c] '#{text}' || value contains[c] '#{text}'";
-    var a = w.elements().withPredicate(search).toArray();
+  js = all_ele_js "name contains[c] '#{text}' || label contains[c] '#{text}' || value contains[c] '#{text}'"
 
-    if ( a.length === 0 ) {
-      a = [];
-    }
+  execute_script js
+end
 
-    au._returnElems($(a));
-  )
+# Return the first element matching text.
+# @param text [String] the text to search for
+# @return [Element] the first matching element
+def text text
+  # TODO: Use XPath index once it's implemented
+  # https://github.com/appium/appium/issues/295
+  js = first_ele_js "value contains[c] '#{text}'"
+
+  execute_script(js).first
+end
+
+# Return all elements matching text.
+# @param text [String] the text to search for
+# @return [Array<Element>] all matching elements
+def texts text
+  # XPath //* is not implemented on iOS
+  # https://github.com/appium/appium/issues/430
+    js = all_ele_js "value contains[c] '#{text}'"
+
+  execute_script js
+end
+
+# Return the first element matching name.
+# on Android name is content description
+# on iOS name is the accessibility label or the text.
+# @param name [String] the name to search for
+# @return [Element] the first matching element
+def name text
+  js = first_ele_js "name contains[c] '#{text}' || label contains[c] '#{text}'"
+
+  execute_script(js).first
+end
+
+# Return all elements matching name.
+# on Android name is content description
+# on iOS name is the accessibility label or the text.
+# @param name [String] the name to search for
+# @return [Array<Element>] all matching elements
+def names text
+  # find_elements :name is not the same as on Android.
+  # it's case sensitive and exact on iOS and not on Android.
+  # https://github.com/appium/appium/issues/379
+  js = all_ele_js "name contains[c] '#{text}' || label contains[c] '#{text}''"
 
   execute_script js
 end
