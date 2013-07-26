1f0428dca4e843a82de2d46be3c724e2dc7930c7
bootstraponline <code@bootstraponline.com>
2013-07-26 13:09:16 -0400

    Fix keyboard race condition
    After typing, the keyboard does not instantly appear. Wait for it to appear, and
    then dismiss the keyboard.

--- lib/appium_lib/ios/patch.rb
+++ lib/appium_lib/ios/patch.rb
@@ -16,16 +16,22 @@ now swipe down until the end of the window - 10 pixels.
 
 Swiping inside the keyboard will not dismiss it.
 =end
-        js = <<-JS
-          au.getElement('#{self.ref}').setValue('#{text}');
+        # type
+        execute_script %(au.getElement('#{self.ref}').setValue('#{text}');)
 
+        # wait for keyboard
+        wait_true { execute_script %(au.mainApp.keyboard().type() !== 'UIAElementNil') }
+
+        # dismiss keyboard
+        js = <<-JS
           if (au.mainApp.keyboard().type() !== "UIAElementNil") {
             var startY = au.mainApp.keyboard().rect().origin.y - 10;
             var endY = au.mainWindow.rect().size.height - 10;
             au.flickApp(0, startY, 0, endY);
           }
         JS
-        @driver.execute_script js
+
+        execute_script js
       end
     end
   end
