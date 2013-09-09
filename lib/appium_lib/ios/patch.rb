b611ac6c361b7044d07d2501ae032784aa891c66
bootstraponline <code@bootstraponline.com>
2013-09-09 10:25:53 -0400

    Fix typing on disabled textfield
    Fix #86

--- lib/appium_lib/ios/patch.rb
+++ lib/appium_lib/ios/patch.rb
@@ -19,20 +19,25 @@ Swiping inside the keyboard will not dismiss it.
         # type
         $driver.execute_script %(au.getElement('#{self.ref}').setValue('#{text}');)
 
-        # wait for keyboard
-        $driver.wait_true { $driver.execute_script %(au.mainApp.keyboard().type
-() !== 'UIAElementNil') }
+        $driver.ignore {
+          # wait 5 seconds for keyboard. if the textfield is disabled then
+          # setValue will work, however the keyboard will never display
+          # because users are normally not allowed to type into it.
+          $driver.wait_true(5) do
+            $driver.execute_script %(au.mainApp.keyboard().type() !== 'UIAElementNil')
+          end
 
-        # dismiss keyboard
-        js = <<-JS
-          if (au.mainApp.keyboard().type() !== "UIAElementNil") {
-            var startY = au.mainApp.keyboard().rect().origin.y - 10;
-            var endY = au.mainWindow.rect().size.height - 10;
-            au.flickApp(0, startY, 0, endY);
-          }
-        JS
+          # dismiss keyboard
+          js = <<-JS
+            if (au.mainApp.keyboard().type() !== "UIAElementNil") {
+              var startY = au.mainApp.keyboard().rect().origin.y - 10;
+              var endY = au.mainWindow.rect().size.height - 10;
+              au.flickApp(0, startY, 0, endY);
+            }
+          JS
 
-        $driver.execute_script js
+          $driver.execute_script js
+        }
       end
     end
   end
