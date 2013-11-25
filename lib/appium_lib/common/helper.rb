77a7736c3589db6ba2f029b97beae3e0d383cee9
bootstraponline <code@bootstraponline.com>
2013-11-25 09:09:28 -0500

    wait and wait_true no longer use default_wait
    Waits are easier to understand if the default wait is a static 30 seconds.

--- lib/appium_lib/common/helper.rb
+++ lib/appium_lib/common/helper.rb
@@ -30,15 +30,7 @@ module Appium::Common
   # @param interval [Float] the time in seconds to wait after calling the block
   # @param block [Block] the block to call
   # @return [Object] the result of block.call
-  def wait max_wait=-1, interval=0.5, &block
-    if max_wait == -1
-      max_wait = begin
-        $driver.default_wait
-        rescue
-      end
-      max_wait ||= 30
-    end
-
+  def wait max_wait=30, interval=0.5, &block
     max_wait = 1 if max_wait <= 0
     result = nil
     timeout max_wait do
@@ -60,15 +52,7 @@ module Appium::Common
   # @param interval [Float] the time in seconds to wait after calling the block
   # @param block [Block] the block to call
   # @return [Object] the result of block.call
-  def wait_true max_wait=-1, interval=0.5, &block
-    if max_wait == -1
-      max_wait = begin
-        $driver.default_wait
-        rescue
-      end
-      max_wait ||= 30
-    end
-
+  def wait_true max_wait=30, interval=0.5, &block
     max_wait = 1 if max_wait <= 0
     result = nil
     timeout max_wait do
