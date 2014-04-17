af76d4daba1cc785325e62f4a1392895da7c7731
bootstraponline <code@bootstraponline.com>
2014-04-17 15:43:44 -0400

    Fix broken ends

--- lib/appium_lib/common/patch.rb
+++ lib/appium_lib/common/patch.rb
@@ -44,123 +44,123 @@ module Appium
                        y: "#{center_y} / #{w.height.to_f}")
       end
     end
-  end # module Appium::Common
+  end # module Common
+end # module Appium
 
 # Print JSON posted to Appium. Not scoped to an Appium module.
 #
 # Requires from lib/selenium/webdriver/remote.rb
-  require 'selenium/webdriver/remote/capabilities'
-  require 'selenium/webdriver/remote/bridge'
-  require 'selenium/webdriver/remote/server_error'
-  require 'selenium/webdriver/remote/response'
-  require 'selenium/webdriver/remote/commands'
-  require 'selenium/webdriver/remote/http/common'
-  require 'selenium/webdriver/remote/http/default'
+require 'selenium/webdriver/remote/capabilities'
+require 'selenium/webdriver/remote/bridge'
+require 'selenium/webdriver/remote/server_error'
+require 'selenium/webdriver/remote/response'
+require 'selenium/webdriver/remote/commands'
+require 'selenium/webdriver/remote/http/common'
+require 'selenium/webdriver/remote/http/default'
 
 # @private
 # Show http calls to the Selenium server.
 #
 # Invaluable for debugging.
-  def patch_webdriver_bridge
-    Selenium::WebDriver::Remote::Bridge.class_eval do
-      # Code from lib/selenium/webdriver/remote/bridge.rb
-      def raw_execute(command, opts = {}, command_hash = nil)
-        verb, path          = Selenium::WebDriver::Remote::COMMANDS[command] || raise(ArgumentError, "unknown command: #{command.inspect}")
-        path                = path.dup
+def patch_webdriver_bridge
+  Selenium::WebDriver::Remote::Bridge.class_eval do
+    # Code from lib/selenium/webdriver/remote/bridge.rb
+    def raw_execute(command, opts = {}, command_hash = nil)
+      verb, path          = Selenium::WebDriver::Remote::COMMANDS[command] || raise(ArgumentError, "unknown command: #{command.inspect}")
+      path                = path.dup
 
-        path[':session_id'] = @session_id if path.include?(':session_id')
+      path[':session_id'] = @session_id if path.include?(':session_id')
 
-        begin
-          opts.each { |key, value| path[key.inspect] = escaper.escape(value.to_s) }
-        rescue IndexError
-          raise ArgumentError, "#{opts.inspect} invalid for #{command.inspect}"
-        end
-
-        # convert /// into /
-        path.gsub! /\/+/, '/'
-
-        # change path from session/efac972c-941a-499c-803c-d7d008749/execute
-        # to /execute
-        # path may be nil, session, or not have anything after the session_id.
-        path_str   = path
-        path_str   = '/' + path_str unless path_str.nil? ||
-            path_str.length <= 0 || path_str[0] == '/'
-        path_match = path.match /.*\h{8}-?\h{4}-?\h{4}-?\h{4}-?\h{12}/
-        path_str   = path.sub(path_match[0], '') unless path_match.nil?
-
-        puts "#{verb} #{path_str}"
-        unless command_hash.nil? || command_hash.length == 0
-          print_command = {}
-          # For complex_find, we pass a whole array
-          if command_hash.kind_of? Array
-            print_command[:args]   = [command_hash.clone]
-            print_command[:script] = 'mobile: find' if command == :complex_find
-          else
-            print_command = command_hash.clone
-          end
-          print_command.delete :args if print_command[:args] == []
-
-          mobile_find = 'mobile: find'
-          if print_command[:script] == mobile_find
-            args = print_command[:args]
-            puts "#{mobile_find}" #" #{args}"
-
-            # [[[[3, "sign"]]]] => [[[3, "sign"]]]
-            #
-            # [[[[4, "android.widget.EditText"], [7, "z"]], [[4, "android.widget.EditText"], [3, "z"]]]]
-            # => [[[4, "android.widget.EditText"], [7, "z"]], [[4, "android.widget.EditText"], [3, "z"]]]
-            args       = args[0]
-            option     = args[0].to_s.downcase
-            has_option = !option.match(/all|scroll/).nil?
-            puts option if has_option
-
-            start = has_option ? 1 : 0
-
-            start.upto(args.length-1) do |selector_index|
-              selectors      = args[selector_index]
-              selectors_size = selectors.length
-              selectors.each_index do |pair_index|
-                pair = selectors[pair_index]
-                res  = $driver.dynamic_code_to_string pair[0], pair[1]
-
-                if selectors_size == 1 || pair_index >= selectors_size - 1
-                  puts res
-                elsif selectors_size > 1 && pair_index < selectors_size
-                  print res + '.'
-                end
-              end
-            end # start.upto
-          else
-            ap print_command
-          end
-        end
-        delay = $driver.global_webdriver_http_sleep
-        sleep delay if !delay.nil? && delay > 0
-        # puts "verb: #{verb}, path #{path}, command_hash #{command_hash.to_json}"
-        http.call verb, path, command_hash
-      end # def
-    end # class
-  end
-
-  # def
-
-# Print Appium's origValue error messages.
-  class Selenium::WebDriver::Remote::Response
-    # @private
-    def error_message
-      val = value
-
-      case val
-        when Hash
-          msg = val['origValue'] || val['message'] or return 'unknown error'
-          msg << " (#{ val['class'] })" if val['class']
-        when String
-          msg = val
-        else
-          msg = "unknown error, status=#{status}: #{val.inspect}"
+      begin
+        opts.each { |key, value| path[key.inspect] = escaper.escape(value.to_s) }
+      rescue IndexError
+        raise ArgumentError, "#{opts.inspect} invalid for #{command.inspect}"
       end
 
-      msg
+      # convert /// into /
+      path.gsub! /\/+/, '/'
+
+      # change path from session/efac972c-941a-499c-803c-d7d008749/execute
+      # to /execute
+      # path may be nil, session, or not have anything after the session_id.
+      path_str   = path
+      path_str   = '/' + path_str unless path_str.nil? ||
+          path_str.length <= 0 || path_str[0] == '/'
+      path_match = path.match /.*\h{8}-?\h{4}-?\h{4}-?\h{4}-?\h{12}/
+      path_str   = path.sub(path_match[0], '') unless path_match.nil?
+
+      puts "#{verb} #{path_str}"
+      unless command_hash.nil? || command_hash.length == 0
+        print_command = {}
+        # For complex_find, we pass a whole array
+        if command_hash.kind_of? Array
+          print_command[:args]   = [command_hash.clone]
+          print_command[:script] = 'mobile: find' if command == :complex_find
+        else
+          print_command = command_hash.clone
+        end
+        print_command.delete :args if print_command[:args] == []
+
+        mobile_find = 'mobile: find'
+        if print_command[:script] == mobile_find
+          args = print_command[:args]
+          puts "#{mobile_find}" #" #{args}"
+
+          # [[[[3, "sign"]]]] => [[[3, "sign"]]]
+          #
+          # [[[[4, "android.widget.EditText"], [7, "z"]], [[4, "android.widget.EditText"], [3, "z"]]]]
+          # => [[[4, "android.widget.EditText"], [7, "z"]], [[4, "android.widget.EditText"], [3, "z"]]]
+          args       = args[0]
+          option     = args[0].to_s.downcase
+          has_option = !option.match(/all|scroll/).nil?
+          puts option if has_option
+
+          start = has_option ? 1 : 0
+
+          start.upto(args.length-1) do |selector_index|
+            selectors      = args[selector_index]
+            selectors_size = selectors.length
+            selectors.each_index do |pair_index|
+              pair = selectors[pair_index]
+              res  = $driver.dynamic_code_to_string pair[0], pair[1]
+
+              if selectors_size == 1 || pair_index >= selectors_size - 1
+                puts res
+              elsif selectors_size > 1 && pair_index < selectors_size
+                print res + '.'
+              end
+            end
+          end # start.upto
+        else
+          ap print_command
+        end
+      end
+      delay = $driver.global_webdriver_http_sleep
+      sleep delay if !delay.nil? && delay > 0
+      # puts "verb: #{verb}, path #{path}, command_hash #{command_hash.to_json}"
+      http.call verb, path, command_hash
+    end # def
+  end # class
+end
+
+# def
+
+# Print Appium's origValue error messages.
+class Selenium::WebDriver::Remote::Response
+  # @private
+  def error_message
+    val = value
+
+    case val
+      when Hash
+        msg = val['origValue'] || val['message'] or return 'unknown error'
+        msg << " (#{ val['class'] })" if val['class']
+      when String
+        msg = val
+      else
+        msg = "unknown error, status=#{status}: #{val.inspect}"
     end
+
+    msg
   end
 end
--- lib/appium_lib/device/device.rb
+++ lib/appium_lib/device/device.rb
@@ -59,7 +59,6 @@ module Appium
     #  hide_keyboard # Close a keyboard with the 'Done' button
     #  hide_keyboard('Finished') # Close a keyboard with the 'Finished' button
     #  ```
-
     class << self
       def extended(mod)
         extend_webdriver_with_forwardable
@@ -107,144 +106,146 @@ module Appium
         add_endpoint_method(:hide_keyboard, 'session/:session_id/appium/device/hide_keyboard') do
           def hide_keyboard(close_key='Done')
             execute :hide_keyboard, {}, keyName: close_key
+          end
+        end
 
-            add_endpoint_method(:complex_find, 'session/:session_id/appium/app/complex_find') do
-              def complex_find(all, selectors=nil)
-                if selectors.nil?
-                  selectors = all.dup
-                  all       = false
-                end
-
-                selector_array = all ? ['all'] : []
-                selector_array.push selectors
-
-                ids = execute :complex_find, {}, [selectors]
-                if all && ids.length > 1
-
-                  return ids.map { |id| Selenium::WebDriver::Element.new self, element_id_from(id) }
-                else
-                  return Selenium::WebDriver::Element.new self, element_id_from(ids)
-                end
-              end
+        add_endpoint_method(:complex_find, 'session/:session_id/appium/app/complex_find') do
+          def complex_find(all, selectors=nil)
+            if selectors.nil?
+              selectors = all.dup
+              all       = false
             end
 
-            extend_search_contexts
-          end
+            selector_array = all ? ['all'] : []
+            selector_array.push selectors
 
-          # @private
-          def add_endpoint_method(method, path, verb=:post)
-            if block_given?
-              # &Proc.new with no args passes the passed_in block
-              # Because creating Procs from blocks is slow
-              create_bridge_command method, verb, path, &Proc.new
+            ids = execute :complex_find, {}, [selectors]
+            if all && ids.length > 1
+
+              return ids.map { |id| Selenium::WebDriver::Element.new self, element_id_from(id) }
             else
-              create_bridge_command method, verb, path
-            end
-
-            delegate_driver_method method
-            delegate_appium_driver_method method
-          end
-
-          # @private
-          def extend_webdriver_with_forwardable
-            return if Selenium::WebDriver::Driver.kind_of? Forwardable
-            Selenium::WebDriver::Driver.class_eval do
-              extend Forwardable
-            end
-          end
-
-          # @private
-          def delegate_driver_method(method)
-            return if Selenium::WebDriver::Driver.method_defined? method
-            Selenium::WebDriver::Driver.class_eval { def_delegator :@bridge, method }
-          end
-
-          # @private
-          def delegate_appium_driver_method(method)
-            def_delegator :@driver, method
-          end
-
-          # @private
-          def create_bridge_command(method, verb, path)
-            # Don't clobber methods that are moved into Selenium
-            if selenium_has method
-              log_reimplemented_warning(method, path)
-              return
-            end
-
-            Selenium::WebDriver::Remote::Bridge.class_eval do
-              command method, verb, path
-              if block_given?
-                class_eval &Proc.new
-              else
-                define_method(method) { execute method }
-              end
-            end
-          end
-
-          # @private
-          def selenium_has(method)
-            Selenium::WebDriver::Remote::Bridge.method_defined? method
-          end
-
-          # @private
-          def log_reimplemented_warning(method, path)
-            msg = "Selenium::WebDriver has now implemented the `#{method}` method."
-            if Selenium::WebDriver::Remote::COMMANDS[method][1] == path
-              msg << " It may no longer function as expected"
-            else
-              msg << " It no longer uses the same endpoint,"
-              msg << " so it probably won't do what you expect anymore."
-            end
-            msg << " Raise an issue at http://www.github.com/appium/ruby_lib if so."
-            Appium::Logger.warn msg
-          end
-
-          # @!method accessiblity_id_find
-          #   find_element/s with their accessibility_id
-          #
-          #   ```ruby
-          #    find_elements :accessibility_id, 'Animation'
-          #   ```
-          def extend_search_contexts
-            Selenium::WebDriver::SearchContext.class_eval do
-              Selenium::WebDriver::SearchContext::FINDERS[:accessibility_id] = 'accessibility id'
+              return Selenium::WebDriver::Element.new self, element_id_from(ids)
             end
           end
         end
 
-        # Perform a block within the given context, then switch back to the starting context.
-        # @param context (String) The context to switch to for the duration of the block.
-        #
-        # ```ruby
-        # within_context('NATIVE_APP') do
-        #   find_element [:tag, "button"]
-        # ```
-        def within_context(context)
-          existing_context = current_context
-          yield if block_given?
-          current_context = existing_context
-        end
-
-        # Change to the default context.  This is equivalent to `current_context= nil`.
-        def switch_to_default_context
-          current_context = nil
-        end
-
-        # @!method current_context=
-        #   Change the context to the given context.
-        #   @param [String] The context to change to
-        #
-        #   ```ruby
-        #   current_context= "NATIVE_APP"
-        #   ```
-
-        # @!method current_context
-        #   @return [String] The context currently being used.
-
-        # @!method available_contexts
-        # @return [Array<String>] All usable contexts, as an array of strings
+        extend_search_contexts
       end
-    end
-  end
-end
+
+      # def extended
+
+      # @private
+      def add_endpoint_method(method, path, verb=:post)
+        if block_given?
+          # &Proc.new with no args passes the passed_in block
+          # Because creating Procs from blocks is slow
+          create_bridge_command method, verb, path, &Proc.new
+        else
+          create_bridge_command method, verb, path
+        end
+
+        delegate_driver_method method
+        delegate_appium_driver_method method
+      end
+
+      # @private
+      def extend_webdriver_with_forwardable
+        return if Selenium::WebDriver::Driver.kind_of? Forwardable
+        Selenium::WebDriver::Driver.class_eval do
+          extend Forwardable
+        end
+      end
+
+      # @private
+      def delegate_driver_method(method)
+        return if Selenium::WebDriver::Driver.method_defined? method
+        Selenium::WebDriver::Driver.class_eval { def_delegator :@bridge, method }
+      end
+
+      # @private
+      def delegate_appium_driver_method(method)
+        def_delegator :@driver, method
+      end
+
+      # @private
+      def create_bridge_command(method, verb, path)
+        # Don't clobber methods that are moved into Selenium
+        if selenium_has method
+          log_reimplemented_warning(method, path)
+          return
+        end
+
+        Selenium::WebDriver::Remote::Bridge.class_eval do
+          command method, verb, path
+          if block_given?
+            class_eval &Proc.new
+          else
+            define_method(method) { execute method }
+          end
+        end
+      end
+
+      # @private
+      def selenium_has(method)
+        Selenium::WebDriver::Remote::Bridge.method_defined? method
+      end
+
+      # @private
+      def log_reimplemented_warning(method, path)
+        msg = "Selenium::WebDriver has now implemented the `#{method}` method."
+        if Selenium::WebDriver::Remote::COMMANDS[method][1] == path
+          msg << " It may no longer function as expected"
+        else
+          msg << " It no longer uses the same endpoint,"
+          msg << " so it probably won't do what you expect anymore."
+        end
+        msg << " Raise an issue at http://www.github.com/appium/ruby_lib if so."
+        Appium::Logger.warn msg
+      end
+
+      # @!method accessiblity_id_find
+      #   find_element/s with their accessibility_id
+      #
+      #   ```ruby
+      #    find_elements :accessibility_id, 'Animation'
+      #   ```
+      def extend_search_contexts
+        Selenium::WebDriver::SearchContext.class_eval do
+          Selenium::WebDriver::SearchContext::FINDERS[:accessibility_id] = 'accessibility id'
+        end
+      end
+
+      # Perform a block within the given context, then switch back to the starting context.
+      # @param context (String) The context to switch to for the duration of the block.
+      #
+      # ```ruby
+      # within_context('NATIVE_APP') do
+      #   find_element [:tag, "button"]
+      # ```
+      def within_context(context)
+        existing_context = current_context
+        yield if block_given?
+        current_context = existing_context
+      end
+
+      # Change to the default context.  This is equivalent to `current_context= nil`.
+      def switch_to_default_context
+        current_context = nil
+      end
+
+      # @!method current_context=
+      #   Change the context to the given context.
+      #   @param [String] The context to change to
+      #
+      #   ```ruby
+      #   current_context= "NATIVE_APP"
+      #   ```
+
+      # @!method current_context
+      #   @return [String] The context currently being used.
+
+      # @!method available_contexts
+# @return [Array<String>] All usable contexts, as an array of strings
+    end # class << self
+  end # module Device
+end # module Appium
