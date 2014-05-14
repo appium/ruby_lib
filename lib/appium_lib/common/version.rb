2d7ab8b6cc442f39f6171f8a27bc4923e4d4e2a4
bootstraponline <code@bootstraponline.com>
2014-05-14 17:03:06 -0400

    Release 2.0.0

--- lib/appium_lib/common/version.rb
+++ lib/appium_lib/common/version.rb
@@ -1,5 +1,5 @@
 module Appium
   # Version and Date are defined on the 'Appium' module, not 'Appium::Common'
-  VERSION = '1.0.0' unless defined? ::Appium::VERSION
-  DATE    = '2014-04-29' unless defined? ::Appium::DATE
+  VERSION = '2.0.0' unless defined? ::Appium::VERSION
+  DATE    = '2014-05-14' unless defined? ::Appium::DATE
 end
