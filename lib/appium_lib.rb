543f8f38763bec0d468dcadb3c4a6facf734e569
kieferrm <kieferrm@gmail.com>
2014-06-05 13:06:20 -0700

    change require to lower case
    Change require to use correct case of file name to ensure loading works on case-sensitive file systems.

--- lib/appium_lib.rb
+++ lib/appium_lib.rb
@@ -2,7 +2,7 @@
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-require 'Forwardable' unless defined? Forwardable
+require 'forwardable' unless defined? Forwardable
 require_relative 'appium_lib/rails/duplicable'
 
 $driver = nil
