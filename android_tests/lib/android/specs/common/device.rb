9838d6a5d910632a96df906c8d15a6084be97ea2
bootstraponline <code@bootstraponline.com>
2014-06-30 11:43:09 -0400

    Move context tests to web_context.rb

--- android_tests/lib/android/specs/common/device.rb
+++ android_tests/lib/android/specs/common/device.rb
@@ -33,60 +33,6 @@ describe 'common/device' do
     wait { current_activity.must_equal '.ApiDemos' }
   end
 
-  # appium's context support is broken on android
-
-  t 'available_contexts' do
-    wait_true { available_contexts.include? 'NATIVE_APP' }
-  end
-
-  t 'current_context' do
-    wait { current_context.must_equal 'NATIVE_APP' }
-  end
-
-  t 'set_context' do
-    wait { scroll_to('Views').click }
-    wait { scroll_to('WebView').click }
-
-    def undo_setcontext_nav
-      back
-      wait { find('WebView') }
-      back
-      wait { find'Views' }
-    end
-
-    contexts = available_contexts
-    webview_context = contexts.detect { |e| e.start_with?('WEBVIEW') }
-
-    if webview_context.nil?
-      undo_setcontext_nav
-      raise "No webview context found. contexts are: #{contexts}"
-    end
-
-    wait { set_context webview_context }
-    wait { current_context.must_equal webview_context }
-
-    wait { set_context 'NATIVE_APP' }
-    wait { current_context.must_equal 'NATIVE_APP' }
-
-    undo_setcontext_nav
-  end
-
-  t 'within_context' do
-    $driver.within_context 'NATIVE_APP' do
-      wait { current_context.must_equal 'NATIVE_APP' }
-    end
-  end
-
-  t 'switch_to_default_context' do
-    wait do
-      switch_to_default_context
-      current_context.must_equal 'NATIVE_APP'
-    end
-
-    wait { set_context 'NATIVE_APP' }
-    wait { current_context.must_equal 'NATIVE_APP' }
-  end
-
   t 'app_strings' do
     wait_true { app_strings.has_key? 'activity_save_restore' }
   end
--- android_tests/lib/android/specs/common/web_context.rb
+++ android_tests/lib/android/specs/common/web_context.rb
@@ -2,16 +2,60 @@
 # rake android[common/web_context]
 describe 'the web context' do
 
-  t 'get_android_inspect' do
-    scroll_to 'Views'
-    last_text.click
-    scroll_to 'WebView'
-    last_text.click
+  # appium's context support is broken on android
 
-    webview_context = available_contexts.detect { |e| e.start_with?('WEBVIEW') }
+  t 'available_contexts' do
+    wait_true { available_contexts.include? 'NATIVE_APP' }
+  end
 
-    set_context webview_context
-    current_context.must_equal webview_context
+  t 'current_context' do
+    wait { current_context.must_equal 'NATIVE_APP' }
+  end
+
+  t 'set_context' do
+    wait { scroll_to('Views').click }
+    wait { scroll_to('WebView').click }
+
+    def undo_setcontext_nav
+      back
+      wait { find('WebView') }
+      back
+      wait { find 'Views' }
+    end
+
+    contexts        = available_contexts
+    webview_context = contexts.detect { |e| e.start_with?('WEBVIEW') }
+
+    if webview_context.nil?
+      undo_setcontext_nav
+      raise "No webview context found. contexts are: #{contexts}"
+    end
+
+    wait { set_context webview_context }
+    wait { current_context.must_equal webview_context }
+
+    # verify inspect within webview works
     get_android_inspect.split("\n").length.must_be :>=, 3
+
+    wait { set_context 'NATIVE_APP' }
+    wait { current_context.must_equal 'NATIVE_APP' }
+
+    undo_setcontext_nav
+  end
+
+  t 'within_context' do
+    $driver.within_context 'NATIVE_APP' do
+      wait { current_context.must_equal 'NATIVE_APP' }
+    end
+  end
+
+  t 'switch_to_default_context' do
+    wait do
+      switch_to_default_context
+      current_context.must_equal 'NATIVE_APP'
+    end
+
+    wait { set_context 'NATIVE_APP' }
+    wait { current_context.must_equal 'NATIVE_APP' }
   end
 end
