24b75d70f52cf5e2fe090086ed2ee5280cb572bd
bootstraponline <code@bootstraponline.com>
2014-06-30 10:56:08 -0400

    Update alert tests for Android L

--- android_tests/lib/android/specs/android/element/alert.rb
+++ android_tests/lib/android/specs/android/element/alert.rb
@@ -1,7 +1,8 @@
+# rake android[android/element/alert]
 describe 'android/element/alert' do
   def open_alert
     # trigger the alert
-    wait { button('OK Cancel dialog with a long message').click }
+    wait { button('OK Cancel dialog with a message').click }
   end
 
   t 'alert_click' do
@@ -11,7 +12,7 @@ describe 'android/element/alert' do
 
     # test
     wait { open_alert }
-    wait { alert_click('Something') }
+    wait { alert_click('OK') }
   end
 
   t 'alert_accept' do
@@ -32,6 +33,12 @@ describe 'android/element/alert' do
 
   t 'alert_dismiss_text' do
     wait { open_alert }
+
+    # alert dialog button placement changed:
+    # 'OK Cancel dialog with a long message'
+    # Android L: 'Something, Cancel, Ok'
+    # Android 19: 'Cancel, Something, Ok'
+
     wait { alert_dismiss_text.must_equal 'Cancel' }
     wait { alert_dismiss }
 
