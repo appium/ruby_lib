e5b3939068f3fcd65ecd04f3f6fdd8652a159c43
bootstraponline <code@bootstraponline.com>
2014-07-01 15:03:50 -0400

    Update install test on Android

--- android_tests/lib/android/specs/install.rb
+++ android_tests/lib/android/specs/install.rb
@@ -1,10 +1,23 @@
 describe 'install' do
-  # To Do: Blocked on https://github.com/appium/appium/issues/3030
-=begin
+  # To Do: Blocked on https://github.com/appium/appium/issues/3032
+  def pkg
+    'io.appium.android.apis'
+  end
+
+  def installed
+    is_installed?(pkg).must_equal true
+  end
+
+  def not_installed
+    is_installed?(pkg).must_equal false
+  end
+
   t 'install/uninstall/launch' do
+    installed
     remove 'io.appium.android.apis'
+    not_installed
     install caps[:app]
+    installed
     launch
   end
-=end
 end
