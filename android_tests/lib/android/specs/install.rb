e02498c59272a173aed2eca662ef0c38539b6b24
bootstraponline <code@bootstraponline.com>
2014-07-01 14:18:37 -0400

    Fix #152

---
+++ android_tests/lib/android/specs/install.rb
@@ -0,0 +1,10 @@
+describe 'install' do
+  # To Do: Blocked on https://github.com/appium/appium/issues/3030
+=begin
+  t 'install/uninstall/launch' do
+    remove 'io.appium.android.apis'
+    install caps[:app]
+    launch
+  end
+=end
+end
