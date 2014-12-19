ddd94b498601bfd961dc449f00e584ed6bf753d8
bootstraponline <code@bootstraponline.com>
2014-12-18 21:15:02 -0500

    Remove method_missing

--- lib/appium_lib.rb
+++ lib/appium_lib.rb
@@ -5,24 +5,8 @@ Encoding.default_internal = Encoding::UTF_8
 require 'forwardable' unless defined? Forwardable
 require_relative 'appium_lib/rails/duplicable'
 
+# Init global driver
 $driver = nil
-
-# @private
-# Invoke top level methods on last created Appium driver.
-def self.method_missing method, *args, &block
-  raise "driver is nil. called #{method}" if $driver == nil
-
-  if $driver.respond_to?(method)
-    # puts "[method_missing] Calling driver.send for #{method}"
-    $driver.send(method, *args, &block)
-  elsif self.respond_to?(method)
-    # puts "[method_missing] Calling super with args for #{method}"
-    super(*args, &block)
-  else
-    # puts "[method_missing] Calling super (no args) for #{method}"
-    super
-  end
-end
 
 require_relative 'appium_lib/logger'
 require_relative 'appium_lib/driver'
