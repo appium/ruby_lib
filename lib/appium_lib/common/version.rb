2a9f79caae337e8770aa56c47d0bd9c17cf1569f
Chris Kruger <montdidier@users.noreply.github.com>
2016-10-16 23:40:06 +0800

    swipe proffers use of delta_x, delta_y instead of end_x, end_y which … (#380)
    * swipe proffers use of delta_x, delta_y instead of end_x, end_y which are marked as deprecated
    
    * stylistic compliance

--- lib/appium_lib/common/version.rb
+++ lib/appium_lib/common/version.rb
@@ -1,5 +1,5 @@
 module Appium
   # Version and Date are defined on the 'Appium' module, not 'Appium::Common'
-  VERSION = '8.0.2' unless defined? ::Appium::VERSION
-  DATE    = '2016-01-29' unless defined? ::Appium::DATE
+  VERSION = '8.0.3' unless defined? ::Appium::VERSION
+  DATE    = '2016-10-12' unless defined? ::Appium::DATE
 end
