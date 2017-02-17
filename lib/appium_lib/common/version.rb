422a4683f10fc99d3731985d532b71e1fe80b4e6
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2017-02-18 01:01:30 +0900

    Release 9 3 3 (#494)
    * Release 9.3.3
    
    * Update release notes
    
    * update changelog

--- lib/appium_lib/common/version.rb
+++ lib/appium_lib/common/version.rb
@@ -1,5 +1,5 @@
 module Appium
   # Version and Date are defined on the 'Appium' module, not 'Appium::Common'
-  VERSION = '9.3.2'.freeze unless defined? ::Appium::VERSION
-  DATE    = '2017-02-11'.freeze unless defined? ::Appium::DATE
+  VERSION = '9.3.3'.freeze unless defined? ::Appium::VERSION
+  DATE    = '2017-02-18'.freeze unless defined? ::Appium::DATE
 end
