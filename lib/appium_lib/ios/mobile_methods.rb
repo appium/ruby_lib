c5eb3d074898222c4e73e998b4b96df2908315a3
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-03-22 23:00:06 +0900

    fix rubocop

--- lib/appium_lib/ios/mobile_methods.rb
+++ lib/appium_lib/ios/mobile_methods.rb
@@ -22,9 +22,12 @@ module Appium
       #   find_element/s can be used with a [class chain]( https://github.com/facebook/WebDriverAgent/wiki/Queries)
       #
       #   ```ruby
-      #    find_elements :class_chain, 'XCUIElementTypeWindow/XCUIElementTypeButton[3]' # select the third child button of the first child window element
-      #    find_elements :class_chain, 'XCUIElementTypeWindow' # select all the children windows
-      #    find_elements :class_chain, 'XCUIElementTypeWindow[2]/XCUIElementTypeAny[-2]' # select the second last child of the second child window
+      #    # select the third child button of the first child window element
+      #    find_elements :class_chain, 'XCUIElementTypeWindow/XCUIElementTypeButton[3]'
+      #    # select all the children windows
+      #    find_elements :class_chain, 'XCUIElementTypeWindow'
+      #    # select the second last child of the second child window
+      #    find_elements :class_chain, 'XCUIElementTypeWindow[2]/XCUIElementTypeAny[-2]'
       #   ```
       def extended(_mod)
         ::Appium::Driver::SearchContext::FINDERS[:uiautomation] = '-ios uiautomation'
