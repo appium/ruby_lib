72610858d1cfc272f39de2d270d9caae0b476042
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-03-22 22:58:28 +0900

    add class chain

--- lib/appium_lib/ios/mobile_methods.rb
+++ lib/appium_lib/ios/mobile_methods.rb
@@ -16,9 +16,20 @@ module Appium
       #    find_elements :predicate, 'wdName == "Buttons"'
       #    find_elements :predicate, 'wdValue == "SearchBar" AND isWDDivisible == 1'
       #   ```
+      #
+      # @!method ios_class_chain_find
+      #   Only for XCUITest(WebDriverAgent)
+      #   find_element/s can be used with a [class chain]( https://github.com/facebook/WebDriverAgent/wiki/Queries)
+      #
+      #   ```ruby
+      #    find_elements :class_chain, 'XCUIElementTypeWindow/XCUIElementTypeButton[3]' # select the third child button of the first child window element
+      #    find_elements :class_chain, 'XCUIElementTypeWindow' # select all the children windows
+      #    find_elements :class_chain, 'XCUIElementTypeWindow[2]/XCUIElementTypeAny[-2]' # select the second last child of the second child window
+      #   ```
       def extended(_mod)
         ::Appium::Driver::SearchContext::FINDERS[:uiautomation] = '-ios uiautomation'
         ::Appium::Driver::SearchContext::FINDERS[:predicate] = '-ios predicate string'
+        ::Appium::Driver::SearchContext::FINDERS[:class_chain] = '-ios class chain'
       end
     end # class << self
   end # module Ios
