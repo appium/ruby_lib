292822cb15e25621012ee91d0ead131ad28a46c3
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-03-23 23:45:58 +0900

    add commands

--- lib/appium_lib/common/command.rb
+++ lib/appium_lib/common/command.rb
@@ -13,8 +13,12 @@ module Appium
         open_notifications:         [:post, 'session/:session_id/appium/device/open_notifications'.freeze],
         toggle_airplane_mode:       [:post, 'session/:session_id/appium/device/toggle_airplane_mode'.freeze],
         current_activity:           [:get,  'session/:session_id/appium/device/current_activity'.freeze],
+        get_system_bars:            [:get,  'session/:session_id/appium/device/system_bars'.freeze],
+        get_display_density:        [:get,  'session/:session_id/appium/device/display_density'.freeze],
+        is_keyboard_shown:          [:get,  'session/:session_id/appium/device/is_keyboard_shown'.freeze],
         get_network_connection:     [:get,  'session/:session_id/network_connection'.freeze],
         get_performance_data_types: [:post, 'session/:session_id/appium/performanceData/types'.freeze],
+
 
         # iOS
         device_time:                [:get,  'session/:session_id/appium/device/system_time'.freeze],
