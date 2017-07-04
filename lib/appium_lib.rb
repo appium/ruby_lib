6c5e99e7b30f65a0c7b5a393d57cf2855a52ff65
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-07-05 08:27:44 +0900

    remove rails/duplicable and related methods

--- lib/appium_lib.rb
+++ lib/appium_lib.rb
@@ -3,7 +3,6 @@ Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
 require 'forwardable' unless defined? Forwardable
-require_relative 'appium_lib/rails/duplicable'
 
 # Init global driver
 $driver = nil
