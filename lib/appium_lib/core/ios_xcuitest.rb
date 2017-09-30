7708429e22ac7fb8b59fa9f69e89e3e01b3e24fe
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-09-30 17:40:38 +0900

    remove require uiautomation

--- lib/appium_lib/core/ios_xcuitest.rb
+++ lib/appium_lib/core/ios_xcuitest.rb
@@ -1,7 +1,6 @@
 # loaded in common/driver.rb
 require_relative 'ios/search_context'
 require_relative 'ios/device'
-require_relative 'ios/uiautomation/patch'
 
 require_relative 'ios/xcuitest/search_context'
 require_relative 'ios/xcuitest/device'
