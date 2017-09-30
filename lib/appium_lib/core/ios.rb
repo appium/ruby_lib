0d47e453668e1234ae2ef7862a09470acd1fb2fd
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-09-30 17:34:58 +0900

    define patch module

--- lib/appium_lib/core/ios.rb
+++ lib/appium_lib/core/ios.rb
@@ -1,6 +1,6 @@
 # loaded in common/driver.rb
 require_relative 'ios/search_context'
 require_relative 'ios/device'
-require_relative 'ios/patch'
 
+require_relative 'ios/uiautomation/patch'
 require_relative 'ios/uiautomation/bridge'
--- lib/appium_lib/core/ios_xcuitest.rb
+++ lib/appium_lib/core/ios_xcuitest.rb
@@ -1,7 +1,7 @@
 # loaded in common/driver.rb
 require_relative 'ios/search_context'
 require_relative 'ios/device'
-require_relative 'ios/patch'
+require_relative 'ios/uiautomation/patch'
 
 require_relative 'ios/xcuitest/search_context'
 require_relative 'ios/xcuitest/device'
