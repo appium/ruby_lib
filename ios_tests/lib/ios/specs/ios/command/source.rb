b89eaa70c8d2605773637896d152aed6d0999684
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-10-13 02:13:05 +0900

    add todo

--- ios_tests/lib/ios/specs/ios/command/source.rb
+++ ios_tests/lib/ios/specs/ios/command/source.rb
@@ -1,8 +1,7 @@
 # rake ios[ios/command/source]
 describe 'ios/command/source' do
   t 'source' do
-    # TODO: need assert
-    xcuitest_source format: :json
-    xcuitest_source format: :xml
+    xcuitest_source(format: :json).is_a?(Hash).must_equal true
+    xcuitest_source(format: :xml).is_a?(String).must_equal true
   end
 end
