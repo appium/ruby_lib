9af174fd343481f2b38cd96344a7c24c071e5695
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2017-10-12 09:26:36 +0900

    docs: update docs for search context (#700)

--- lib/appium_lib/core/ios/xcuitest/search_context.rb
+++ lib/appium_lib/core/ios/xcuitest/search_context.rb
@@ -7,18 +7,24 @@ module Appium
           #   Only for XCUITest(WebDriverAgent)
           #   find_element/s can be used with a [class chain]( https://github.com/facebook/WebDriverAgent/wiki/Queries)
           #
-          #   ```ruby
-          #    # select the third child button of the first child window element
-          #    find_elements :class_chain, 'XCUIElementTypeWindow/XCUIElementTypeButton[3]'
-          #    # select all the children windows
-          #    find_elements :class_chain, 'XCUIElementTypeWindow'
-          #    # select the second last child of the second child window
-          #    find_elements :class_chain, 'XCUIElementTypeWindow[2]/XCUIElementTypeAny[-2]'
-          #    # matching predicate. <code>`</code> is the mark.
-          #    find_elements :class_chain, 'XCUIElementTypeWindow[`visible = 1][`name = \"bla\"`]'
-          #    # containing predicate. `$` is the mark.
-          #    find_elements :class_chain, 'XCUIElementTypeWindow[$name = \"bla$$$bla\"$]'
-          #   ```
+          # @example
+          #
+          #   # select the third child button of the first child window element
+          #   find_elements :class_chain, 'XCUIElementTypeWindow/XCUIElementTypeButton[3]'
+          #
+          #   # select all the children windows
+          #   find_elements :class_chain, 'XCUIElementTypeWindow'
+          #
+          #   # select the second last child of the second child window
+          #   find_elements :class_chain, 'XCUIElementTypeWindow[2]/XCUIElementTypeAny[-2]'
+          #
+          #   # matching predicate. <code>`</code> is the mark.
+          #   find_elements :class_chain, 'XCUIElementTypeWindow[`visible = 1][`name = \"bla\"`]'
+          #
+          #   # containing predicate. `$` is the mark.
+          #   # Require appium-xcuitest-driver 2.54.0+
+          #   find_elements :class_chain, 'XCUIElementTypeWindow[$name = \"bla$$$bla\"$]'
+          #
           def self.extend
             ::Appium::Core::Base::SearchContext.add_finders(class_chain: '-ios class chain')
           end
