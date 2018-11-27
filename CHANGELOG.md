fa8d4ed2aaa5259a1fa744be955555cf775ead3e
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-11-28 00:24:19 +0900

    Release 9.16.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,8 +3,14 @@ Commit based release not is [release_notes.md](./release_notes.md)
 
 Release tags are https://github.com/appium/ruby_lib/releases .
 
-
 ## Unreleased
+### 1. Enhancements
+
+### 2. Bug fixes
+
+### 3. Deprecations
+
+## v9.16.1
 ### 1. Enhancements
 
 ### 2. Bug fixes
--- lib/appium_lib/version.rb
+++ lib/appium_lib/version.rb
@@ -1,5 +1,5 @@
 module Appium
   # Version and Date are defined on the 'Appium' module, not 'Appium::Common'
-  VERSION = '9.16.0'.freeze unless defined? ::Appium::VERSION
-  DATE    = '2018-11-14'.freeze unless defined? ::Appium::DATE
+  VERSION = '9.16.1'.freeze unless defined? ::Appium::VERSION
+  DATE    = '2018-11-28'.freeze unless defined? ::Appium::DATE
 end
