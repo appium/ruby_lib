abee8eea787b276cb4487c47befc736147722b26
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-03-19 00:45:16 -0700

    replace

--- ios_tests/lib/ios/specs/common/command/command.rb
+++ ios_tests/lib/ios/specs/common/command/command.rb
@@ -13,17 +13,23 @@
 # limitations under the License.
 
 # rake "ios[common/command/command]"
-describe 'common/command/command' do
-  it 'command' do
-    File.delete 'syslog.log' if File.exist? 'syslog.log'
+class IosTest
+  class Common
+    class Command
+      class Command < Minitest::Test
+        def test_01_command
+          File.delete 'syslog.log' if File.exist? 'syslog.log'
 
-    # A number of systemlog is quite small than Android.
-    # It only checks the commands works fine.
-    start_logs_broadcast
-    sleep 5
-    assert stop_logs_broadcast.nil?
+          # A number of systemlog is quite small than Android.
+          # It only checks the commands works fine.
+          start_logs_broadcast
+          sleep 5
+          assert stop_logs_broadcast.nil?
 
-    assert File.exist?('syslog.log') if File.exist? 'syslog.log'
-    assert File.size?('syslog.log') if File.exist? 'syslog.log'
+          assert File.exist?('syslog.log') if File.exist? 'syslog.log'
+          assert File.size?('syslog.log') if File.exist? 'syslog.log'
+        end
+      end
+    end
   end
 end
